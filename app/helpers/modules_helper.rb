2856f3261a70029ae45f864681597c7178a8bc15
Christian Gambardella <christian.gambardella@gmail.com>
2014-06-12 15:22:43 +0200

    nice_select dom changes

--- app/helpers/modules_helper.rb
+++ app/helpers/modules_helper.rb
@@ -2,9 +2,10 @@ module ModulesHelper
 
   def nice_select(select)
     raise "I need a select tag" if select.blank?
-    label = content_tag(:span, "label", class: "select-label")
+    label_value = content_tag(:span, "label", class: "select-label-value")
     arrow = content_tag(:span, nil, class: "select-arrow")
-    select_content = label + arrow + select
+    label = content_tag(:span, (label_value + arrow), class: "select-label")
+    select_content = label + select
     content_tag(:div, select_content, class: "nice-select")
   end
   
