c036bf05b1ced85924f73cae86d3caa7b9437357
Christian Gambardella <christian.gambardella@gmail.com>
2014-06-16 16:31:12 +0200

    registrations form ajaxified

--- app/controllers/authentication/registrations_controller.rb
+++ app/controllers/authentication/registrations_controller.rb
@@ -23,6 +23,53 @@ class Authentication::RegistrationsController < Devise::RegistrationsController
   #
   #
   
+
+  def create
+    build_resource(sign_up_params)
+
+    if resource.save
+      logger.debug { "---------------- SAVED" }
+      respond_to do |format|
+        format.html {
+          logger.debug { "---------------- HTML" }
+          yield resource if block_given?
+          if resource.active_for_authentication?
+            logger.debug { "------------ OBEN" }
+            set_flash_message :notice, :signed_up if is_flashing_format?
+            sign_up(resource_name, resource)
+            respond_with resource, :location => after_sign_up_path_for(resource)
+          else
+            logger.debug { "------------ UNTEN" } 
+            set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+            expire_data_after_sign_in!
+            respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+          end
+        }
+        format.js {
+          logger.debug { "---------------- HTML" }
+          flash[:notice] = "Created account, signed in."
+          render :template => "devise/registrations/create"
+          flash.discard
+          # sign_up(resource_name, resource)
+        }
+      end
+    else
+      logger.debug { "---------------- NOT SAVED" }
+      respond_to do |format|
+        format.html {
+          clean_up_passwords resource
+          respond_with resource
+        }
+        format.js {
+          flash[:alert] = @user.errors.full_messages.to_sentence
+          render template: "devise/registrations/new"
+          flash.discard
+        }
+      end
+    end
+  end
+
+
   #
   # Protected
   # ---------------------------------------------------------------------------------------
