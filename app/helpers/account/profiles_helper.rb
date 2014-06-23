1d07d8a01472e68ac540dcfe3a7afd6c952ec930
Christian Gambardella <christian.gambardella@gmail.com>
2014-06-23 18:41:28 +0200

    soundcloud iframe parsing, soundcloud from db

--- app/helpers/account/profiles_helper.rb
+++ app/helpers/account/profiles_helper.rb
@@ -7,5 +7,28 @@ module Account::ProfilesHelper
     method = profile.publishable? ? :put : :get
     link_to "Publish", url, method: method, class: css_class, disabled: !profile.publishable?
   end
+
+  def has_soundcloud?
+    !!soundcloud_id_from_iframe(resource.soundcloud)
+  end
+
+  def soundcloud_widget(iframe = nil)
+    iframe = resource.soundcloud if iframe.blank?
+    track_id = soundcloud_id_from_iframe(iframe)
+    logger.debug { "FOUND TRACK ID: #{track_id}" }
+    return nil if track_id.blank?
+    iframe_src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/#{track_id}&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=true&amp;visual=true"
+    content_tag :iframe, nil, width: "100%", height: "350", scrolling: "no", frameborder: "no", src: iframe_src
+  end
+
+private
+
+  def soundcloud_id_from_iframe(iframe)
+    begin
+      iframe.scan(/https%3A\/\/api.soundcloud.com\/tracks\/(\d*)/)[0][0]  
+    rescue Exception => e
+      return nil
+    end
+  end
   
 end
