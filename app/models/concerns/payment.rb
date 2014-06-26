5142766af5e570acba18fe3bc92ff740fe86ce90
shirzad <shirzad.kamawall@gmail.com>
2014-06-26 17:21:09 +0200

    show saved credit card info on payment page

--- app/models/concerns/payment.rb
+++ app/models/concerns/payment.rb
@@ -4,6 +4,39 @@ module Payment
   included do
   end
   
+  def credit_last4
+    credit_card["last4"]
+  end
+  
+  def credit_type
+    credit_card["type"]
+  end
+  
+  def credit_exp_month
+    I18n.t("date.abbr_month_names")[credit_card["exp_month"].to_i]
+  end
+  
+  def credit_exp_year
+    credit_card["exp_year"]
+  end
+  
+  def credit_card
+    @card ||= retrieve_credit_card
+    @card.first
+  end
+  
+  def retrieve_credit_card
+    begin
+       Stripe::Customer.retrieve(stripe_customer_id).cards
+    rescue Stripe::StripeError => e
+      User.where(id: id).update_all(stripe_log: e.json_body.inspect)
+      Rails.logger.info "###########################"
+      Rails.logger.info e.inspect
+      Rails.logger.info "###########################"
+      return [{}]
+    end
+  end
+  
   def create_stripe_customer(user, desc=nil)
     begin
       Stripe::Customer.create(card: user.stripe_token, description: desc || user.email)
