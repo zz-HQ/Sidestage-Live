5133fdd209ab90f962a526f48cfb04b8313605b7
Silab Kamawall <silab.kamawall@gmail.com>
2014-07-02 14:46:44 +0200

    new genres

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,15 +11,34 @@ User.connection.execute("TRUNCATE users")
 User.connection.execute("TRUNCATE profiles")
 
 Genre.connection.execute("TRUNCATE genres")
-["Classical", "Party DJ", "Alternative", "Acappella","Acid Jazz","Afro-Brazilian","Ambient","Americana","Avant-Garde","Ballroom Dance","Bar Band",
-  "Baroque","Bluegrass","Blues","Brass Band","Cabaret","Celtic","Chamber Music","Chanukah","Children's",
-  "Classical Guitar","Club/Dance","Conceptual Art","Country","Disco","Early Music","Eastern Europe",
-  "Easy Listening","Electro","Electro-Techno","Electronic","Electronica","Emo","Ethnic","Experimental","Folk","Funk",
-  "Gangsta Rap","German Schlager","German Volksmusik","Gospel","Grunge","Heavy Metal","Hip-Hop","House","Indie Rock",
-  "Jam Bands","Jazz","Jewish Music","Latin","Lounge","Military","Minimal Techno","Motown","New Age","Noise","Opera",
-  "Pipe Bands","Punk","R&B","Ragtime","Rap","Reggae","Rock","Rock & Roll","Romantic","Salsa","Samba",
-  "Singer/Songwriter","Soft Rock","Solo Instrumental","Steel Band","Tech-House","Techno","Traditional",
-  "Traditional Native American","Trance","Turkish Music","Urban","Waltz","Wedding Music"].each do |g|
+["Acappella",
+"Baroque",
+"Bluegrass",
+"Blues",
+"Classical",
+"Country",
+"Electronic",
+"Ethnic",
+"Experimental",
+"Folk",
+"Funk",
+"Gospel",
+"Hip hop",
+"Indie",
+"Jazz",
+"Latin",
+"Minimal Techno",
+"Opera",
+"Other",
+"Party DJ",
+"Ragtime",
+"Reggae",
+"Rock",
+"Singer/Songwriter",
+"Techno",
+"Traditional",
+"Wedding Music"
+].each do |g|
   Genre.create(name: g)
 end
 
