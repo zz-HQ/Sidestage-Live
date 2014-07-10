b20cefb422d62ac73e1cd69e8d7b77861683af5d
Silab Kamawall <silab.kamawall@gmail.com>
2014-07-10 16:26:03 +0200

    homepage online

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Airmusic::Application.routes.draw do
   
 
   scope '(:locale)', locale: Regexp.new(I18n.available_locales.map(&:to_s).join('|'))   do
-    root to: "home#index"
+    root to: "home#homepage"
 
     post 'create_subscriber', to: "home#create_subscriber", as: "create_subscriber"
 
