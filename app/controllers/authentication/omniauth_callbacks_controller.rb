d29b6cedaa574c3734736c080f022660f00aaf39
Silab Kamawall <silab.kamawall@gmail.com>
2014-07-10 17:41:21 +0200

    fb connect redirect stuff

--- app/controllers/authentication/omniauth_callbacks_controller.rb
+++ app/controllers/authentication/omniauth_callbacks_controller.rb
@@ -5,9 +5,12 @@ class Authentication::OmniauthCallbacksController < Devise::OmniauthCallbacksCon
     logger.debug { "------ facebook #{request.env["omniauth.auth"]}" }
     if @user.persisted?
       assign_potential_profile(@user)
-      store_location(complete_pricing_account_profile_path(@user.profile)) if @user.profile.present?
+      store_location_for(:user, complete_pricing_account_profile_path(@user.profile)) if @user.profile.present?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      logger.debug { "--------------- START" }
+      session.delete(stored_location_key_for(:user))
+      logger.debug { "--------------- END" }
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
