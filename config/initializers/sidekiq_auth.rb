09bd9c7c0c92934e54062b37f766d741bf6aab58
Christian Gambardella <christian.gambardella@gmail.com>
2014-07-28 15:15:24 +0200

    Sidekiq Monitoring

--- config/initializers/sidekiq_auth.rb
+++ config/initializers/sidekiq_auth.rb
@@ -1,5 +1,8 @@
 # # -*- encoding : utf-8 -*-
-# require 'sidekiq/web'
-# Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-#   username == 'admin' && password == 'chigga030'
-# end
+require 'sidekiq/web'
+
+unless Rails.env.development?
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == 'admin' && password == 'forthewin'
+  end
+end
