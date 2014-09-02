8d1ec1a2a92d080d5476104ff371fa48b9aba5c0
shirzad <shirzad.kamawall@gmail.com>
2014-09-02 17:50:27 +0200

    Regex

---
+++ app/services/regular_expressions.rb
@@ -0,0 +1,53 @@
+class RegularExpressions
+  
+  PHONE_NR_PATTERN = /\b([\+][0-9]{1,3}[ \.\-]?)?([\(]{1}[0-9]{1,6}[\)])?([0-9 \.\-\/]{4,20})((x|ext|extension)[ ]?[0-9]{1,4})?\b/
+
+  EMAIL_PATTERN = /\b([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\b/i
+
+  URL_PATTERN = %r{
+    \b
+
+    # protocol identifier
+    (?:(?:https?|ftp)://)
+
+    # user:pass authentication
+    (?:\S+(?::\S*)?@)?
+
+    (?:
+      # IP address exclusion
+      # private & local networks
+      (?!10(?:\.\d{1,3}){3})
+      (?!127(?:\.\d{1,3}){3})
+      (?!169\.254(?:\.\d{1,3}){2})
+      (?!192\.168(?:\.\d{1,3}){2})
+      (?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})
+
+      # IP address dotted notation octets
+      # excludes loopback network 0.0.0.0
+      # excludes reserved space >= 224.0.0.0
+      # excludes network & broacast addresses
+      # (first & last IP address of each class)
+      (?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])
+      (?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}
+      (?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))
+    |
+      # host name
+      (?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)
+
+      # domain name
+      (?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*
+
+      # TLD identifier
+      (?:\.(?:[a-z\u00a1-\uffff]{2,}))
+    )
+
+    # port number
+    (?::\d{2,5})?
+
+    # resource path
+    (?:/[^\s]*)?
+
+    \b
+  }xi
+
+end
