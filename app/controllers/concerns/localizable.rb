97ca606895a31c9410492709ed32dab527297470
shirzad <shirzad.kamawall@gmail.com>
2014-10-02 15:52:05 +0200

    diverse fixes

--- app/controllers/concerns/localizable.rb
+++ app/controllers/concerns/localizable.rb
@@ -51,7 +51,7 @@ module Localizable
   end
   
   def default_url_options(options = {})
-    options.merge(:locale => I18n.locale)
+    options.merge(:locale =>  I18n.locale == I18n.default_locale ? nil : I18n.locale)
   end
     
 end
