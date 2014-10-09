ac7acb8776395053098dec702a69513886c3d422
shirzad <webmaster@tolafghan.com>
2014-10-09 23:15:55 +0200

    teests

--- spec/features/coupons_spec.rb
+++ spec/features/coupons_spec.rb
@@ -11,38 +11,35 @@ feature "coupon", js: true do
     
     describe "Customer enters coupon on profile page" do
       
-      # it "renders error" do
-      #   gaga = FactoryGirl.create(:gaga)
-      #   login_as(current_user)
-      #     
-      #   visit artist_path(gaga, locale: :en)
-      #   click_button "Book it"
-      #       
-      #   click_link "coupon_show"
-      #   fill_in "coupon_code", with: "COUPON"
-      #   click_button "coupon_apply"
-      # 
-      #   expect(page).to have_css("#coupon_failure", visible: true)
-      #   expect(page).to have_css("#coupon_success", visible: false)
-      # end
+      it "shows error" do
+        gaga = FactoryGirl.create(:gaga)
+        login_as(current_user)
+          
+        visit artist_path(gaga, locale: :en)
+        click_button "Book it"
+            
+        click_link "Coupon"
+        fill_in "deal[coupon_code]", with: "C"
+        click_button "coupon_apply"
+      
+        expect(page).to have_css("[data-coupon=failure]", visible: true)
+        expect(page).to have_css("[data-coupon=success]", visible: false)
+      end
 
-      it "renders success" do
+      it "hides original price and shows coupon price" do
         gaga = FactoryGirl.create(:gaga)
         coupon = FactoryGirl.create(:coupon)
         login_as(current_user)
     
         visit artist_path(gaga, locale: :en)
         click_button "Book it"
-        
-        find(:xpath, "//a[contains(@data-coupon, 'show')]").click()
-        find(:xpath, "//input[contains(@data-coupon, 'code')]").fill_in with: coupon.code}
-#        click_link "[data-coupon='show']"
-#        fill_in "//input[contains(@data-coupon, 'code')]", with: coupon.code
-#        fill_in "[data-coupon=code]", with: coupon.code
-        click_button "coupon_apply"
 
-        expect(page).to have_css("[data-coupon=failure]", visible: false)
-        expect(page).to have_css("[data-coupon=success]", visible: true)
+        click_link "Coupon"
+        fill_in "deal[coupon_code]", with: coupon.code
+        click_button "coupon_apply"
+        
+        expect(page).to have_css("[data-coupon=original_price]", visible: false)
+        expect(page).to have_css("[data-coupon=coupon_price]", visible: true)
       end
 
       
