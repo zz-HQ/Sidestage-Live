3326df5d010bd8bc0aed1263768c463ee8a9d377
shirzad <webmaster@tolafghan.com>
2014-11-12 23:16:31 +0100

    different tasks

--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -23,5 +23,16 @@ class EmailWorker
     return if event_invitation.nil?
     EventInvitationMailer.host_acceptance_confirmation(event_invitation).deliver
   end
+  
+  def profile_published_confirmation(profile_id)
+    profile = Profile.where(id: profile_id).first
+    UserMailer.profile_published_confirmation(profile).deliver
+  end
+
+  def welcome_artist(profile_id)
+    profile = Profile.where(id: profile_id).first
+    UserMailer.welcome_artist(profile).deliver
+  end
+
 
 end
