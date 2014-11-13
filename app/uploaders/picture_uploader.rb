0026a242ee0426cec5a108187af5c19bb2825d62
Yousef Kamawall <yousef.kamawall@gmail.com>
2014-11-13 13:15:12 +0100

    picture size fix!

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,12 +2,21 @@
 class PictureUploader < Uploader
 
   version :preview do
-    process :resize_to_fit => [620, nil]
+    process :resize_to_fit => [800, nil]
+  end
+
+  version :large do
+    process :resize_to_fill => [376, 310]
+  end
+
+  version :medium_preview do
+    process :resize_to_fill => [203, 167]
   end
 
   version :medium_rect do
-    process :resize_to_fill => [376, 310]
+    process :resize_to_fill => [300, 300]
   end
+  # die Version wird nicht gebraucht, aber um kein Error in partial: '_picture' zu bekommen
 
   version :thumb do
     process :resize_to_fill => [100, 100]
