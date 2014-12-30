aca79bbedf2d15051541c1cb80a1124779c9120d
shirzad <webmaster@tolafghan.com>
2014-12-30 22:19:53 +0100

    admin search refactored, minor bug fixes

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -18,14 +18,22 @@ class Admin::DashboardController < Admin::BaseController
   #
   #
   #  
-    
+
   def global_search
-   @elastic = {}
+   @elastic = []
    if params[:term].present?
-     @elastic = Elasticsearch::Model.multi_model_search([User], query: { match:  { _all: { query: params[:term], operator: :and } } }, size: 100).results #, from: 12)
-   end   
-   render json: @elastic.collect { |r| { target: r.artist? ? artist_url(r.profile.slug||r.profile.id) : root_url, label: "<img class='thumb' src='#{r.avatar_mini_url}'> #{r.artist? ? "Artist" : "User"} : #{r.full_name}".html_safe, value: r.id } }
+     @elastic = Elasticsearch::Model.multi_model_search([User], query: { match:  { _all: { query: params[:term], operator: :and } } }, size: 100) #, from: 12)
+   end
+   
   end
+    
+  # def global_search
+  #  @elastic = {}
+  #  if params[:term].present?
+  #    @elastic = Elasticsearch::Model.multi_model_search([User], query: { match:  { _all: { query: params[:term], operator: :and } } }, size: 100).results #, from: 12)
+  #  end
+  #  render json: @elastic.collect { |r| { target: r.artist? ? artist_url(r.profile.slug||r.profile.id) : root_url, label: "<img class='thumb' src='#{r.avatar_mini_url}'> #{r.artist? ? "Artist" : "User"} : #{r.full_name}".html_safe, value: r.id } }
+  # end
   
   
   
