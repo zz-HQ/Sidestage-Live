a619dba1b6381b8a6fbe4abcea9178bce8d1aac8
David Anderson <david@alpinegizmo.com>
2015-01-20 11:10:49 +0100

    added delete user option to user details page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < Admin::ResourcesController
-  
-  
+
+
   #
   # Actions
   # ---------------------------------------------------------------------------------------
@@ -8,28 +8,28 @@ class Admin::UsersController < Admin::ResourcesController
   #
   #
   #
-  
+
   def destroy
     destroy! do |success, failure|
-      success.html{ redirect_to :back }
+      success.html{ redirect_to admin_users_path }
     end
   end
-  
+
   def confirm
     resource.confirm!
-    redirect_to :back  
+    redirect_to :back
   end
-  
+
   def toggle_verification
     resource.update_attribute :verified, !resource.verified?
-    redirect_to :back 
+    redirect_to :back
   end
-  
+
   def backdoor
     sign_out(:user)
     warden.request.env['devise.skip_trackable'] = true
     sign_in(resource)
     redirect_to after_sign_in_path_for(resource)
   end
-  
+
 end
